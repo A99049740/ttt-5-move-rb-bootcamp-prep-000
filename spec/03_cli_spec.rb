e69aded0ec8e5a7544f2840fccb9cc119b955da8
jill zhang <talktojillzhang@gmail.com>
2018-09-05 05:33:17 +0000

    automatically backed up by learn

--- spec/03_cli_spec.rb
+++ spec/03_cli_spec.rb
@@ -1,13 +1,12 @@
-require_relative "../lib/move.rb"
-require_relative "spec_helper.rb"
-
+#require_relative "../lib/move.rb"
+require_relative "./spec_helper.rb"
 describe './bin/move executing a CLI Application' do
   it 'defines a board variable' do
     allow($stdout).to receive(:puts)
     allow(self).to receive(:gets).and_return("1")
     allow(self).to receive(:move)
 
-    board = get_variable_from_file("./bin/move", "board")
+    board = get_variable_from_file("./bin/move", "index")
 
     expect(board).to eq([" ", " ", " ", " ", " ", " ", " ", " ", " "])
   end
